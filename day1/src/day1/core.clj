5df75ba0ab3621ecb2c8f0d20cd725d548dee0a9
AndrewSinclair <andrew.george.sinclair@gmail.com>
2017-07-06 23:09:48 -0500

    Add function for parsing string into Instruction record

--- day1/src/day1/core.clj
+++ day1/src/day1/core.clj
@@ -10,6 +10,18 @@
       s/trim
       (s/split #", ")))
 
+(defrecord Instruction [direction distance])
+
+(defn str->instruction
+  [instr]
+  (let [direction (first instr)
+        distance  (->>
+                    instr
+                    rest
+                    (apply str)
+                    Integer.)]
+    (->Instruction direction distance)))
+
 (defn day1
   []
   nil)
