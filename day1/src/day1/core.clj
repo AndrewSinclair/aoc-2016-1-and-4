83d77b476d87a69e693bef40167eac3eccc81878
AndrewSinclair <andrew.george.sinclair@gmail.com>
2017-07-06 22:26:31 -0500

    Add get-input to read puzzle file into strings

--- day1/src/day1/core.clj
+++ day1/src/day1/core.clj
@@ -1,10 +1,22 @@
-(ns day1.core)
+(ns day1.core
+  (:require [clojure.string :as s]))
+
+(def filename "resources/input.txt")
+
+(defn get-input
+  [filename]
+  (-> filename
+      slurp
+      s/trim
+      (s/split #", ")))
 
 (defn day1
   []
   nil)
 
 (defn -main
+  "Advent of Code '16 - Day 1
+  How far away is the HQ?"
   [& args]
   (do
     (println "The answers for day 1 are:")
