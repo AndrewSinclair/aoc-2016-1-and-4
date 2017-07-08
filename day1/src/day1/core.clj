c19592b8dc0764e089e6916d8283e8048bc46232
AndrewSinclair <andrew.george.sinclair@gmail.com>
2017-07-07 20:47:52 -0500

    Add functions to change heading and position

--- day1/src/day1/core.clj
+++ day1/src/day1/core.clj
@@ -3,14 +3,16 @@
 
 (def filename "resources/input.txt")
 
+(defrecord Instruction [direction distance])
+(defrecord Position    [x y])
+(defrecord State       [heading position])
+
 (defn get-input
   [filename]
   (-> filename
       slurp
       s/trim
       (s/split #", ")))
-
-(defrecord Instruction [direction distance])
 
 (defn str->instruction
   [instr]
@@ -21,6 +23,48 @@
                     (apply str)
                     Integer.)]
     (->Instruction direction distance)))
+
+(defn inputs->instructions
+  [inputs]
+  (map ->Instruction inputs))
+
+(defn calc-next-heading
+  [heading direction]
+  (if (= direction \R)
+    (case heading
+      :north :east
+      :east  :south
+      :south :west
+      :west  :north)
+    (case heading
+      :north :west
+      :east  :north
+      :south :east
+      :west  :south)))
+
+(defn calc-next-position
+  [heading {x :x y :y} distance]
+  (case heading
+    :north (->Position x              (+ y distance))
+    :south (->Position x              (- y distance))
+    :east  (->Position (+ x distance) y)
+    :west  (->Position (- x distance) y)))
+
+(defn calc-next-state
+  [{heading   :heading
+    position  :position}
+   {direction :direction
+    distance  :distance}]
+  (let [next-heading
+         (calc-next-heading
+           heading
+           direction)
+        next-position
+         (calc-next-position
+           next-heading
+           position
+           distance)]
+   (->State next-heading next-position)))
 
 (defn day1
   []
