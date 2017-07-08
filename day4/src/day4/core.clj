1974af069ecb0da25c494684db99a85318552c26
AndrewSinclair <andrew.george.sinclair@gmail.com>
2017-07-08 11:30:15 -0500

    Add a checksum validation

--- day4/src/day4/core.clj
+++ day4/src/day4/core.clj
@@ -3,32 +3,49 @@
 
 (def filename "resources/input.txt")
 
+(defrecord Room [cipher-text ordered sector checksum])
+
 (defn order-alphabets
   [encryption]
-    (->>
-      encryption
-      (filter #(Character/isLetter %))
-        sort
-        frequencies
-        (sort-by second >)
-        (map first)))
+  (->>
+    encryption
+    (filter #(Character/isLetter %))
+    sort
+    frequencies
+    (sort-by second >)
+    (map first)))
 
 (defn destruct-line
-    [line]
-    (let [[_ encryption sector checksum]
-            (re-matches #"(.*)-(\d+)\[(\w+)\]" line)]
-      {:cipher-text encryption
-       :ordered    (order-alphabets encryption)
-       :sector     (Integer. sector)
-       :checksum   checksum}))
+  [line]
+  (let [[_ encryption sector checksum]
+       (re-matches #"(.*)-(\d+)\[(\w+)\]" line)]
+    (->Room
+      encryption
+      (order-alphabets encryption)
+      (Integer. sector)
+      checksum)))
 
 (defn parse-input
-    [filename]
+  [filename]
+  (->>
+    filename
+    slurp
+    s/split-lines
+    (map destruct-line)))
+
+(defn check-checksum
+  [room]
+  (let [encryption (take 5 (:ordered room))
+        checksum   (:checksum room)]
     (->>
-      filename
-      slurp
-      s/split-lines
-      (map destruct-line)))
+      (map vector encryption checksum)
+      (every? (partial apply =)))))
+
+(defn filter-real-rooms
+  [rooms]
+  (->>
+    rooms
+    (filter check-checksum)))
 
 (defn part1 [] nil)
 (defn part2 [] nil)
