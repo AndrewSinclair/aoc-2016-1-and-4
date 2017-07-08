e46c5b1a84fc71ac13e7fe0b815c056407943427
AndrewSinclair <andrew.george.sinclair@gmail.com>
2017-07-08 10:28:10 -0500

    Add test generators to create random rooms in correct format

--- day4/test/day4/core_test.clj
+++ day4/test/day4/core_test.clj
@@ -6,6 +6,34 @@
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :refer [defspec]]))
 
+(def room-substring-generator
+  (gen/fmap
+    (comp clojure.string/lower-case (partial apply str))
+    (gen/not-empty
+      (gen/vector gen/char-alpha))))
+
+(def room-cipher-alpha-generator
+  (gen/fmap
+    (partial clojure.string/join "-")
+    (gen/not-empty
+      (gen/vector room-substring-generator))))
+
+(def room-checksum-generator
+  (gen/fmap
+    (comp clojure.string/lower-case (partial apply str))
+    (gen/vector gen/char-alpha 5)))
+
+;; completely random rooms that can be parsed
+;; but are almost for sure not "real rooms"
+(def random-room-code-generator
+  (gen/fmap
+    (fn [[names number checksum]]
+      (str names "-" number "[" checksum "]"))
+    (gen/tuple
+      room-cipher-alpha-generator
+      gen/s-pos-int
+      room-checksum-generator)))
+
 (testing "day4"
   #_(defspec foo-bar)
 )
